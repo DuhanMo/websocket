13906b1ff1d69f326d79d9d352c30b04d8287714
moduhan <devduhanmo@gmail.com>
2022-02-01 23:13:19 +0900

    first-commit

---
+++ src/main/java/com/duhan/websocket/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.duhan.websocket.chat.controller;
+
+import com.duhan.websocket.chat.dto.ChatRoom;
+import com.duhan.websocket.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
