3defd42b90af119724c8f1dfa84cf8ce2a7d8992
moduhan <devduhanmo@gmail.com>
2022-02-02 21:24:46 +0900

    feat: stomp를 이용한 웹소켓채팅

--- src/main/java/com/duhan/websocket/chat/controller/ChatController.java
+++ src/main/java/com/duhan/websocket/chat/controller/ChatController.java
@@ -1,26 +1,22 @@
 package com.duhan.websocket.chat.controller;
 
-import com.duhan.websocket.chat.dto.ChatRoom;
-import com.duhan.websocket.chat.service.ChatService;
+import com.duhan.websocket.chat.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
-import java.util.List;
 
-@RestController
-@RequestMapping("/chat")
 @RequiredArgsConstructor
+@Controller
 public class ChatController {
 
-    private final ChatService chatService;
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
